a47313c64493b3e257eed642fc8963ed7298f232
Naresh Deeti <nareshdeetind@outlook.com>
2023-05-04 20:47:15 +0530

    Enhanced alignment of footer and header

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Currency } from '../common/classes/Currency';
 import { CurrentCurrencyRate, DataInterface } from '../common/classes/CurrencyApiRes'
 
 @Injectable({
